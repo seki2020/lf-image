21fa912a3eb16e910cacef9d20c170a3bbd25554
Jason <ming.m.xi@pwc.com>
2019-07-03 17:03:01 +0900

    ui finish
    todo... show loading when calling
    todo... new Firebase Functions for search

--- src/ImageForm/ImageForm.jsx
+++ src/ImageForm/ImageForm.jsx
@@ -1,5 +1,24 @@
 import React, {Component} from 'react'
-import {Input} from 'semantic-ui-react'
+import {Dropdown,Icon} from 'semantic-ui-react'
+
+import './ImageForm.css'
+
+const methodOptions = [
+    {
+        key: 'Search',
+        text: 'Search',
+        value: 'Search',
+    },
+    {
+        key: 'Input',
+        text: 'Input',
+        value: 'Input',
+    }];
+
+const MODE = {
+    search:'Search',
+    input:'Input'
+}
 
 class ImageForm extends Component {
     constructor(props) {
@@ -7,31 +26,58 @@ class ImageForm extends Component {
         this.onSearch = props.onSearch.bind(this)
         this.pressSearch = this.pressSearch.bind(this)
         this.handleUserInput = this.handleUserInput.bind(this)
+        this.handleModeChange = this.handleModeChange.bind(this)
 
+
+
+        this.state = {
+            modeVal:MODE.search
+        }
     }
 
     handleUserInput(e) {
-        debugger
         this.setState({
             queryUrl: e.target.value
         })
     }
 
     pressSearch() {
-        this.props.onSearch(this.state.queryUrl)
+        if(this.state.modeVal === MODE.input){
+            this.props.onSearch(this.state.queryUrl)
+        }else{
+            alert('Search Mode WIP...')
+        }
 
     }
+    handleModeChange(e,option){
+        this.setState({
+            modeVal:option.value
+        })
+    }
+
 
     render() {
         return (
-            <div className="ui fluid icon input">
-                <button className="ui button" onClick={this.pressSearch}>
-                    <i aria-hidden="true" className="search icon"></i>
-                    Search
-                </button>
-                <input type="text" placeholder="Search..." onChange={this.handleUserInput}/>
 
-            </div>
+                <div className="ui fluid icon input">
+                    <Dropdown
+                        placeholder='Select Friend'
+                        selection
+                        value={this.state.modeVal}
+                        onChange={this.handleModeChange}
+                        options={methodOptions}
+                    />
+
+                    <input type="text"
+                           className='searchInput'
+                           placeholder='...'
+                           onChange={this.handleUserInput}/>
+                    <button className="ui button" onClick={this.pressSearch}>
+                        <i aria-hidden="true" className="search icon"></i>
+                        {this.state.modeVal}
+                    </button>
+                </div>
+
         )
     }
 }
